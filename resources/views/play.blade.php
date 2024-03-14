742fcd138e93abde327e661ab297d0fbcc8fad92
ivan.moreira <peixoto.ivanm@gmail.com>
2024-03-14 19:10:04 -0300

    debug

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -295,7 +295,7 @@
             </div>
             `;
 
-            div.appendChild(signalsDiv);
+            // div.appendChild(signalsDiv);
             let src = null;
             switch (id) {
                 case 'fortune-tiger':
@@ -331,7 +331,7 @@
             iframe.src = src;
             iframe.style.width = '100%';
             iframe.style.height = 'calc(100% - 240px)';
-            iframe.style.top = '60px';
+            iframe.style.top = '0';
             iframe.style.left = '0';
             iframe.style.zIndex = '300';
             iframe.id = 'external-site';
